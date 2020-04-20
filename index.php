c85df16773c9fb5b3bfdec8b0be16ca356f1c283
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-20 17:39:16 +0200

    ADD Simple index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title></title>
+
+    <link rel="stylesheet" href="css/semantic.min.css">
+</head>
+<body>
+
+    <script src="js/semantic.min.js"></script>
+</body>
+</html>
