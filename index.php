a4e6c68382bc9635aed6cf0849154a667c7cb12b
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-21 13:50:01 +0200

    UPDATE index.php add viewport

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="nl">
 <head>
     <title>EenmaalAndermaal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/semantic.min.css">
     <link rel="stylesheet" href="css/stylesheet.css">
