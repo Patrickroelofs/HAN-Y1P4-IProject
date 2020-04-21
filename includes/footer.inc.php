cd105eff9a625c288300203b47102d10c85df50c
Camiel Nijman <camielnijman@gmail.com>
2020-04-21 13:47:01 +0200

    ADD footer and styling

---
+++ includes/footer.inc.php
@@ -0,0 +1,55 @@
+<footer>
+    <section class="primary">
+        <div class="ui container">
+            <div class="centered aligned ui five column grid">
+                <div class="row">
+                    <div class="centered aligned column">
+                        <i class="ui huge centered file image icon"></i>
+                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <i class="ui huge centered file image icon"></i>
+                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <i class="ui huge centered file image icon"></i>
+                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <i class="ui huge centered file image icon"></i>
+                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <i class="ui huge centered file image icon"></i>
+                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="ui container">
+            <div class="footer title">
+                <h1>EenmaalAndermaal</h1>
+            </div>
+
+            <div class="centered aligned ui five column grid">
+                <div class="row">
+                    <div class="centered aligned column">
+                    </div>
+                    <div class="centered aligned column">
+                        <p>Cookies</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <p>Algemene voorwaarden</p>
+                    </div>
+                    <div class="centered aligned column">
+                        <p>Privacy</p>
+                    </div>
+                    <div class="centered aligned column">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
