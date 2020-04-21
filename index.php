b852c9dc1a376cce9a3e2a9b89b89f626c8ca324
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-21 11:02:18 +0200

    ADD Files needed for programming homepage

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-    <title></title>
+    <title>EenmaalAndermaal</title>
 
     <link rel="stylesheet" href="css/semantic.min.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
+    <?php include 'includes/header.inc.php'; ?>
+
+    <div class="ui container">
+        <?php include 'includes/tmp/mainContent.php'; ?>
+    </div>
+
+    <?php include 'includes/footer.inc.php'; ?>
+
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
     <script src="js/semantic.min.js"></script>
