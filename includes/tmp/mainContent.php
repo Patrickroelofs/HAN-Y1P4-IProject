bdbaa7a3bd4b87d82b475551a9b90973ae182804
Rowan-Paul <rpflynn80@gmail.com>
2020-04-21 13:22:08 +0200

    ADD product showcase

--- includes/tmp/mainContent.php
+++ includes/tmp/mainContent.php
@@ -1,5 +1,6 @@
 <div class="ui container">
     <!-- featured content -->
+    <h2>Uitgelicht</h2>
     <div class="ui stackable four column grid">
         <div class="column">
             <div class="ui card">
@@ -43,14 +44,122 @@
         </div>
     </div>
 
-<!--
-    <div class="ui card">
-        <a class="image" href="#">
-            <img src="https://place-hold.it/300x150">
-        </a>
-        <div class="content">
-            <a class="header" href="#">iPhones</a>
+    <!-- Product showcase -->
+    <h2>Producten</h2>
+    <div class="ui stackable five column grid">
+        <div class="five column row">
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="five column row">
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/150x150">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">Product</a>
+                        <div class="description">€52,00</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
--->
 </div>
