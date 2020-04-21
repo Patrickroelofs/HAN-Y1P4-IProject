04b05c0b89992eda0163511d755eccef786a5524
Rowan-Paul <rpflynn80@gmail.com>
2020-04-21 12:30:39 +0200

    ADD highlighted categories

---
+++ includes/tmp/mainContent.php
@@ -0,0 +1,55 @@
+<main>
+    <div class="ui stackable four column grid">
+        <div class="column">
+            <div class="ui card">
+                <a class="image" href="#">
+                    <img src="https://place-hold.it/300x150">
+                </a>
+                <div class="content">
+                    <a class="header" href="#">iPhones</a>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <a class="image" href="#">
+                    <img src="https://place-hold.it/300x150">
+                </a>
+                <div class="content">
+                    <a class="header" href="#">Auto's</a>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <a class="image" href="#">
+                    <img src="https://place-hold.it/300x150">
+                </a>
+                <div class="content">
+                    <a class="header" href="#">Laptops</a>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <a class="image" href="#">
+                    <img src="https://place-hold.it/300x150">
+                </a>
+                <div class="content">
+                    <a class="header" href="#">Mondmaskers</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!--
+    <div class="ui card">
+        <a class="image" href="#">
+            <img src="https://place-hold.it/300x150">
+        </a>
+        <div class="content">
+            <a class="header" href="#">iPhones</a>
+        </div>
+    </div>
+-->
+</main>
