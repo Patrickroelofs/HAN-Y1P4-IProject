e9d6bafab73c20bde73f6c26474c664b71570cbe
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-21 12:55:10 +0200

    ADD temporary first version of the header and searchbar

---
+++ includes/header.inc.php
@@ -0,0 +1,21 @@
+<nav class="white">
+    <div class="ui secondary menu">
+        <div class="ui three column grid container">
+            <div class="item three wide column">
+                <h1>EenmaalAndermaal</h1>
+            </div>
+            <div class="item eight wide column">
+                <div class="ui search fluid">
+                    <div class="ui icon input">
+                        <input class="prompt" type="text" placeholder="Search...">
+                        <i class="search icon"></i>
+                    </div>
+
+                    <div class="results">
+                        <!-- Search results -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -18,5 +18,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
     <script src="js/semantic.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
