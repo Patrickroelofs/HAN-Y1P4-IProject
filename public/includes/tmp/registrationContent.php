8667a0c608d3285cd02f905824e70d36f9cc79d4
BurnedSausage <kseverijns@outlook.com>
2020-04-22 10:24:50 +0200

    ADD registration page

---
+++ public/includes/tmp/registrationContent.php
@@ -0,0 +1,32 @@
+<div class="ui container">
+
+    <div class="ui two column centered grid">
+        <div class="column">
+            <div class="ui segment">
+                <form class="ui large form">
+                    <div class="stacked segment">
+                        <h2 class="text-center">Registreren</h2>
+                        <div class="field">
+                            <label for="username">Gebruikersnaam</label>
+                            <input type="text" name="username" id="username" placeholder="Gebruikersnaam" required>
+                        </div>
+                        <div class="field">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" required>
+                        </div>
+                        <div class="field">
+                            <label for="password">Wachtwoord</label>
+                            <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
+                        </div>
+                        <div class="field">
+                            <label for="repeat_password">Herhaal wachtwoord</label>
+                            <input type="password" name="repeat_password" id="repeat_password" placeholder="Wachtwoord" required>
+                        </div>
+                        <button class="ui fluid large primary submit button">Registreer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
