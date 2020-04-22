c5961b86936499f59fc9532aa8211a02057665b0
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-22 19:56:33 +0200

    UPDATE Executesql.bat

--- build/sql/EXECUTESQL.bat
+++ build/sql/EXECUTESQL.bat
@@ -1,9 +1,42 @@
-:: execute all scripts by calling each script individually
-:: has not been tested when running multiple scripts
-:: SQLCMD -E -dmaster -i (location of script)\actualscript.sql
+:: execute all scripts by calling and running each script individually
 
-SQLCMD -E -dmaster -i H:\HAN-Y1P4-IProject\build\sql\DeleteTables.sql
+:restart
+@ECHO OFF
+ECHO !!!
+ECHO.
+ECHO EXECUTING THIS SCRIPT WILL:
+ECHO - DELETE TABLES
+ECHO - ALL CONTENT
+ECHO.
 PAUSE
+@cls
 
-SQLCMD -E -dmaster -i H:\HAN-Y1P4-IProject\build\sql\createTables\bod.sql
+SQLCMD -i H:\HAN-Y1P4-IProject\build\sql\DELETESCRIPT.sql
+ECHO.
+ECHO STEP 1 - 3
+ECHO DELETED ALL TABLES AND CONTENTS
+ECHO.
 PAUSE
+@cls
+
+SQLCMD -i H:\HAN-Y1P4-IProject\build\sql\CREATESCRIPT.sql
+ECHO.
+ECHO STEP 2 - 3
+ECHO CREATED ALL TABLES
+ECHO.
+PAUSE
+@cls
+
+SQLCMD -i H:\HAN-Y1P4-IProject\build\sql\INSERTSCRIPT.sql
+ECHO.
+ECHO STEP 3 - 3
+ECHO INSERTED ALL DATA INTO TABLES
+ECHO.
+PAUSE
+@cls
+
+:choice
+set /P c=Rerun script[Y/N]?
+if /I "%c%" EQU "Y" goto :restart
+if /I "%c%" EQU "N" goto :choice
+goto :choice
