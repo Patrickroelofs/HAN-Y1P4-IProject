8f34e412b895af208c1f196aa3053825e2e9d257
WiebeBrom <wiebe.bultrug@outlook.com>
2020-04-23 14:21:40 +0200

    UPDATE footer

--- public/includes/footer.inc.php
+++ public/includes/footer.inc.php
@@ -1,27 +1,32 @@
 <footer>
     <section class="primary">
-        <div class="ui container">
+        <div class="ui container selling-point">
             <div class="centered aligned ui five column grid">
                 <div class="row">
                     <div class="centered aligned column">
-                        <i class="ui huge centered file image icon"></i>
-                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                        <i class="ui huge award icon"></i>
+                        <p class="bold">Quality and Saving</p>
+                        <p>Comprehensive quality control and affordable prices</p>
                     </div>
                     <div class="centered aligned column">
-                        <i class="ui huge centered file image icon"></i>
-                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                        <i class="ui huge warehouse icon"></i>
+                        <p class="bold">Global Warehouse</p>
+                        <p>37 overseas warehouses</p>
                     </div>
                     <div class="centered aligned column">
-                        <i class="ui huge centered file image icon"></i>
-                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                        <i class="ui huge truck icon"></i>
+                        <p class="bold">Fast and convenient logistics</p>
+                        <p>Fast and convenient door to door delivery</p>
                     </div>
                     <div class="centered aligned column">
-                        <i class="ui huge centered file image icon"></i>
-                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                        <i class="ui huge euro sign icon"></i>
+                        <p class="bold">Payment Security</p>
+                        <p>More than 40 different secure payment methods</p>
                     </div>
                     <div class="centered aligned column">
-                        <i class="ui huge centered file image icon"></i>
-                        <p class="vertical-padding-12">Lorem Ipsum</p>
+                        <i class="ui huge headset icon"></i>
+                        <p class="bold">Professional service and product warranty</p>
+                        <p>24/7 Customer Service</p>
                     </div>
                 </div>
             </div>
@@ -38,13 +43,13 @@
                     <div class="centered aligned column">
                     </div>
                     <div class="centered aligned column">
-                        <p>Cookies</p>
+                        <a href="index.php">Cookies</a>
                     </div>
                     <div class="centered aligned column">
-                        <p>Algemene voorwaarden</p>
+                        <a href="index.php">Algemene voorwaarden</a>
                     </div>
                     <div class="centered aligned column">
-                        <p>Privacy</p>
+                        <a href="index.php">Privacy</a>
                     </div>
                     <div class="centered aligned column">
                     </div>
