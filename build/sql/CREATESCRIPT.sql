2f6c5b7739eae2b32ddf71d731e6a04388d1ff79
BurnedSausage <kseverijns@outlook.com>
2020-04-23 10:04:29 +0200

    ADD tables
    Checked and working

--- build/sql/CREATESCRIPT.sql
+++ build/sql/CREATESCRIPT.sql
@@ -24,3 +24,68 @@ create table bod
     CONSTRAINT pk_voorwerp_bodBedrag PRIMARY KEY(voorwerp, bodBedrag)
 )
 GO
+
+/*TABLE: Feedback */
+create table tbl_Feedback
+(
+    voorwerp        NUMERIC(10)     NOT NULL,
+    soortGebruiker  CHAR(8)         NOT NULL,
+    feedbacksoort   CHAR(8)         NOT NULL,
+    dag             CHAR(10)        NOT NULL,
+    tijdstip        CHAR(8)         NOT NULL,
+    commentaar      CHAR(12),
+
+    CONSTRAINT pk_voorwerp_soortGebruiker PRIMARY KEY(Voorwerp, SoortGebruiker),
+)
+GO
+
+/* TABLE: Gebruikerstelefoon */
+create table tbl_Gebruikerstelefoon
+(
+    volgnr      INTEGER         NOT NULL,
+    gebruiker   CHAR(10)        NOT NULL,
+    telefoon    CHAR(11)        NOT NULL,
+
+    CONSTRAINT pk_volgnr_gebruiker PRIMARY KEY(Volgnr, Gebruiker)
+)
+GO
+
+/* TABLE: Rubriek */
+create table tbl_Rubriek
+(
+    rubrieknummer   INTEGER      NOT NULL    PRIMARY KEY,
+    rubrieknaam     CHAR(24)     NOT NULL,
+    rubriek         INTEGER,
+    volgnr          INTEGER      NOT NULL
+)
+GO
+
+/* TABLE: Verkoper */
+create table tbl_Verkoper
+(
+    gebruiker       CHAR(10)    NOT NULL    PRIMARY KEY,
+    bank            CHAR(8),
+    bankrekening    INTEGER,
+    controleOptie   CHAR(10)    NOT NULL,
+    creditcard      CHAR(19)
+)
+GO
+
+/*TABLE: Voorwerp in Rubriek*/
+create table tbl_VoorwerpInRubriek
+(
+    voorwerp                    NUMERIC(10)     NOT NULL,
+    rubriekOpLaagsteNiveau      INTEGER         NOT NULL,
+
+    CONSTRAINT pk_Voorwerp_rubriekOpLaagsteNiveau PRIMARY KEY(voorwerp, rubriekOpLaagsteNiveau)
+)
+GO
+
+/* Vraag */
+create table tbl_Vraag
+(
+    vraagnummer     INTEGER     NOT NULL    PRIMARY KEY,
+    tekstVraag      CHAR(21)    NOT NULL
+)
+GO
+
--- build/sql/DELETESCRIPT.sql
+++ build/sql/DELETESCRIPT.sql
@@ -10,3 +10,20 @@ go
 drop table if exists bod
 go
 
+drop table if exists tbl_Feedback
+go
+
+drop table if exists tbl_Gebruikerstelefoon
+go
+
+drop table if exists tbl_Rubriek
+go
+
+drop table if exists tbl_Verkoper
+go
+
+drop table if exists tbl_VoorwerpInRubriek
+go
+
+drop table if exists tbl_Vraag
+go
