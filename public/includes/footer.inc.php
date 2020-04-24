f642938bb30a8b6197bf2bee081ddf75b859225d
Patrick Roelofs <contact@patrickroelofs.com>
2020-04-24 23:29:14 +0200

    FIX Footer responsiveness

--- public/includes/footer.inc.php
+++ public/includes/footer.inc.php
@@ -3,30 +3,40 @@
         <div class="ui container selling-point">
             <div class="centered aligned ui five column grid">
                 <div class="row">
-                    <div class="centered aligned column">
-                        <i class="ui huge award icon"></i>
-                        <p class="bold">Quality and Saving</p>
-                        <p>Comprehensive quality control and affordable prices</p>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <div class="vertical-margin-12">
+                            <i class="ui huge award icon"></i>
+                            <p class="bold">Quality and Saving</p>
+                            <p>Comprehensive quality control and affordable prices</p>
+                        </div>
                     </div>
-                    <div class="centered aligned column">
-                        <i class="ui huge warehouse icon"></i>
-                        <p class="bold">Global Warehouse</p>
-                        <p>37 overseas warehouses</p>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <div class="vertical-margin-12">
+                            <i class="ui huge warehouse icon"></i>
+                            <p class="bold">Global Warehouse</p>
+                            <p>37 overseas warehouses</p>
+                        </div>
                     </div>
-                    <div class="centered aligned column">
-                        <i class="ui huge truck icon"></i>
-                        <p class="bold">Fast and convenient logistics</p>
-                        <p>Fast and convenient door to door delivery</p>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <div class="vertical-margin-12">
+                            <i class="ui huge truck icon"></i>
+                            <p class="bold">Fast and convenient logistics</p>
+                            <p>Fast and convenient door to door delivery</p>
+                        </div>
                     </div>
-                    <div class="centered aligned column">
-                        <i class="ui huge euro sign icon"></i>
-                        <p class="bold">Payment Security</p>
-                        <p>More than 40 different secure payment methods</p>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <div class="vertical-margin-12">
+                            <i class="ui huge euro sign icon"></i>
+                            <p class="bold">Payment Security</p>
+                            <p>More than 40 different secure payment methods</p>
+                        </div>
                     </div>
-                    <div class="centered aligned column">
-                        <i class="ui huge headset icon"></i>
-                        <p class="bold">Professional service and product warranty</p>
-                        <p>24/7 Customer Service</p>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <div class="vertical-margin-12">
+                            <i class="ui huge headset icon"></i>
+                            <p class="bold">Professional service and product warranty</p>
+                            <p>24/7 Customer Service</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -40,18 +50,14 @@
 
             <div class="centered aligned ui five column grid">
                 <div class="row">
-                    <div class="centered aligned column">
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <a href="#">Cookies</a>
                     </div>
-                    <div class="centered aligned column">
-                        <a href="index.php">Cookies</a>
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <a href="#">Algemene voorwaarden</a>
                     </div>
-                    <div class="centered aligned column">
-                        <a href="index.php">Algemene voorwaarden</a>
-                    </div>
-                    <div class="centered aligned column">
-                        <a href="index.php">Privacy</a>
-                    </div>
-                    <div class="centered aligned column">
+                    <div class="centered aligned column sixteen wide mobile tablet three wide computer">
+                        <a href="#">Privacy</a>
                     </div>
                 </div>
             </div>
