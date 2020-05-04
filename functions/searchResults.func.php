8f0beadb4741e0c2f8b3050819babb96906ec064
Rowan-Paul <rpflynn80@gmail.com>
2020-05-04 12:52:02 +0200

    FIX no search results handling

--- functions/searchResults.func.php
+++ functions/searchResults.func.php
@@ -17,7 +17,7 @@ if (isset($_GET['search'])) {
     $results = $stmt->fetch(PDO::FETCH_OBJ);
 
     // TODO: Display results in cards
-    if (!empty($_GET)) {
+    if (empty($_GET)) {
         // no data passed by get
         echo "<p>Geen resultaten</p>";
     }
