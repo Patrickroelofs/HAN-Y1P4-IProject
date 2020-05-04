1ec62c178014227f321cf2bf2ba4956ffe152311
Patrick Roelofs <contact@patrickroelofs.com>
2020-05-04 12:32:26 +0200

    UPDATE index.php add some sections

--- public/index.php
+++ public/index.php
@@ -7,6 +7,28 @@
     <div class="ui container">
         <!-- featured content -->
         <h2>Uitgelicht</h2>
+        <div class="ui stackable two column grid">
+            <div class="column">
+                <div class="ui fluid card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/600x250">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui fluid card">
+                    <a class="image" href="#">
+                        <img src="https://place-hold.it/600x250">
+                    </a>
+                    <div class="content">
+                        <a class="header" href="#">...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="ui stackable four column grid">
             <div class="column">
                 <div class="ui fluid card">
@@ -49,6 +71,71 @@
                 </div>
             </div>
         </div>
+
+
+        <?php if(Session::exists('username')) { ?>
+        <!-- nearby -->
+        <h2>In de buurt</h2>
+        <div class="ui stackable five column grid">
+            <div class="five column row">
+                <div class="column">
+                    <div class="ui fluid card">
+                        <a class="image" href="#">
+                            <img src="https://place-hold.it/150x150">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="#">Product</a>
+                            <div class="description">€52,00</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui fluid card">
+                        <a class="image" href="#">
+                            <img src="https://place-hold.it/150x150">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="#">Product</a>
+                            <div class="description">€52,00</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui fluid card">
+                        <a class="image" href="#">
+                            <img src="https://place-hold.it/150x150">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="#">Product</a>
+                            <div class="description">€52,00</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui fluid card">
+                        <a class="image" href="#">
+                            <img src="https://place-hold.it/150x150">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="#">Product</a>
+                            <div class="description">€52,00</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui fluid card">
+                        <a class="image" href="#">
+                            <img src="https://place-hold.it/150x150">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="#">Product</a>
+                            <div class="description">€52,00</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
 
         <!-- Product showcase -->
         <h2>Producten</h2>
