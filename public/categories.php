aa60c6b24a0b96f796ab176c87eb13d0ae70a413
Patrick Roelofs <contact@patrickroelofs.com>
2020-05-06 11:04:24 +0200

    ADD all categories in list

--- public/categories.php
+++ public/categories.php
@@ -4,14 +4,39 @@
 ?>
 
 <main>
-    <?php
-        $stmt = Database::getInstance()->query("SELECT * FROM Rubriek WHERE rubriek = -1", array());
+    <div class="ui container">
+        <div class="ui container center aligned">
+            <div class="ui three column grid centered stackable">
+                <div class="column">
+                    <div class="ui category search">
+                        <div class="ui icon input fluid">
+                            <input class="prompt" type="text" placeholder="Categorie zoeken...">
+                            <i class="search icon"></i>
+                        </div>
+                        <div class="results"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        foreach($stmt->results() as $result) {
-            echo $result->rubrieknaam;
-            echo '<br>';
-        }
-    ?>
+        <div class="ui stackable four column grid">
+            <?php
+                $categories = Database::getInstance()->query("SELECT * FROM Rubriek WHERE rubriek = -1", array());
+                foreach($categories->results() as $category) {
+            ?>
+            <div class="column">
+                <?php
+                    echo "<a href='category.php?cat=$category->rubrieknummer'>" . $category->rubrieknummer . ' ' . $category->rubrieknaam . '</a><br>';
+
+                    $subcategories = Database::getInstance()->query("SELECT * FROM Rubriek WHERE rubriek = $category->rubrieknummer", array());
+                    foreach($subcategories->results() as $subcategory) {
+                        echo "<a href='category.php?cat=$subcategory->rubrieknummer'>" . $subcategory->rubrieknummer . ' ' . $subcategory->rubrieknaam . '</a><br>';
+                    }
+                ?>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
 </main>
 
 <?php include INCLUDES . 'footer.inc.php'; ?>
