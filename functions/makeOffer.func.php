baf7d469e152ea7212c14258ab85693a5b73a39b
Rowan-Paul <rpflynn80@gmail.com>
2020-05-11 20:17:51 +0200

    UPDATE bidding w/ highest bid from db, insert into Bid table

--- functions/makeOffer.func.php
+++ functions/makeOffer.func.php
@@ -2,25 +2,43 @@
 //======================================================================
 // Offer
 //======================================================================
+$productID = '1';
+
+// Figure out highest bid
+$highestBid = '0';
+
+try {
+    $stmt = Database::getInstance()->query("SELECT TOP 1 bodbedrag FROM Bod WHERE voorwerpnummer = '1' ORDER BY bodbedrag DESC",array());
+
+    foreach($stmt->results() as $result) {
+        $highestBid = $result->bodbedrag;
+    }
+} catch (PDOException $e) {
+    //Error during insert
+    echo $e->getMessage();
+}
+
 
 // Is submit pressed?
 if(isset($_POST['offer-submit'])) {
+    $username = Session::get('username');
 
-    // TODO take product id
     // Save data in temporary variables
     $amount = $_POST['amount'];
 
-    //Insert into database
-    try{
-        $stmt = Database::getInstance()->update('Voorwerp','voorwerpnummer',1,
-        array(
-            'verkoopprijs' => $amount
-        ));
+    // Check if amount is bigger than startprice
+    if ($amount < $highestBid) {
+        echo "Bedrag is te laag";
+    } else {
+        //Insert into database
+        try {
+            $stmt = Database::getInstance()->query("INSERT INTO Bod (voorwerpnummer, bodbedrag, gebruiker) VALUES ($productID, $amount, '".Session::get('username')."')",array());
 
-        Redirect::to('index.php');
+            Redirect::to('index.php');
 
-    } catch(PDOException $e){
-        //Error during insert
-        echo $e->getMessage();
+        } catch (PDOException $e) {
+            //Error during insert
+            echo $e->getMessage();
+        }
     }
 }
