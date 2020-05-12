b3a9febd9c42b57da62b355b0de913e439363b83
Rowan-Paul <rpflynn80@gmail.com>
2020-05-12 13:00:58 +0200

    FIXED missing / in password change

--- public/passwordChange.php
+++ public/passwordChange.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . 'core/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
 if (Session::exists('username')) {
     Redirect::to('index.php');
 }
