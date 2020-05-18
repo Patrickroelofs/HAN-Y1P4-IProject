bb4e178fa9c7adfb1b084de1cd0eb69e035ee66f
WiebeBrom <wiebe.bultrug@outlook.com>
2020-05-18 16:10:56 +0200

    FIX profile picture uploaden

--- functions/profile.func.php
+++ functions/profile.func.php
@@ -54,9 +54,9 @@ if(isset($_POST['update-persoonsgegevens-submit'])) {
     //Randomly hash filename
     $ext = pathinfo($_FILES['profilepicture']['name'], PATHINFO_EXTENSION);
     $filename = md5(basename($profilepicture));
-    $target = '/upload/profilepictures/' . $filename . '.' . $ext;
+    $target = 'upload/profilepictures/' . $filename . '.' . $ext;
     $supported_image = array('gif','jpg','jpeg','png');
-    $size = getimagesize($target);
+    
 
     // TODO: Error messages and other invalid register checks.
     if(empty($firstname) || empty($lastname) || empty($dob)) {
