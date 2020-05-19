f742c31d58c83200ca52295460f040a7ccdcf02e
BurnedSausage <kseverijns@outlook.com>
2020-05-19 14:49:35 +0200

    UPDATE new.php
    Made functional

--- new.php
+++ new.php
@@ -5,7 +5,31 @@ include INCLUDES . 'head.inc.php';
 
     <main>
         <div class="ui container">
+            <h1>Nieuwe producten op de site</h1>
+            <div class="ui stackable five column grid">
+                <?php
+                $newItems = Database::getInstance()->query("SELECT TOP 20 * FROM Items ORDER BY NEWID()");
 
+                if ($newItems->count() < 1) {
+                    // no data passed by get
+                    echo "<p>Geen resultaten</p>";
+                }
+
+                foreach($newItems->results() as $result) { ?>
+                    <div class="column">
+                        <div class="ui fluid card productcards">
+                            <a class="image" href="product.php?p=<?= $result->id; ?>">
+                                <img src="<?= ROOT . $result->thumbnail; ?>" alt="Foto van <?= $result->title; ?>">
+                            </a>
+                            <div class="content">
+                                <a class="header" href="product.php?p=<?= $result->id; ?>"><?= $result->title; ?></a>
+                                <div class="description"><?= $result->description; ?></div>
+                                <div class="description bold">€<?= $result->price; ?></div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
     </main>
 
