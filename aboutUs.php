07ccbdd46fe6f1711c24b3bc6a6276a133b18684
WiebeBrom <wiebe.bultrug@outlook.com>
2020-05-19 14:55:38 +0200

    ADD aboutUs.php

---
+++ aboutUs.php
@@ -0,0 +1,37 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
+include INCLUDES . 'head.inc.php';
+?>
+
+<main>
+    <div class="ui container">
+        <h2>Over Ons</h2>
+        <div class="ui stackable divided grid">
+            <div class="ten wide column">
+                <p>Deze veilingsite is gemaakt in opdracht van het bedrijf iConcepts. Dit bedrijf heeft ervoor gekozen
+                 om de site te laten ontwikkelen door een groep studenten. De reden hiervoor is dat studenten vaak
+                 creatieve ideeën hebben. Dit hebben ze laten zien op de site, die overigens nu voor iedereen toegankelijk is.</p>
+                <h3>Hoe werkt EenmaalAndermaal</h3>
+                <p>U kunt gelijk de hele site bekijken zonder een account te hoeven maken. Wilt u zelf veilingen opzetten dan is
+                     het noodzakelijk dat er een verkopersaccount gemaakt wordt. Dit account is ook nodig voor het aanschaffen van
+                     producten.</p>
+                <p>Zodra dit account gemaakt is kunt u een advertentie plaatsen. Dit kan als u rechtsboven op je account klikt
+                en dan naar 'Advertentie plaatsen' gaat.</p>
+            </div>
+
+            <div class="four wide right floated column">
+            <h3>Bedrijfsgegevens</h3>
+                <b>Adres</b>
+                <p>Ruitenberglaan 31</p>
+                <b>Postcode</b>
+                <p>6826 CC</p>
+                <b>Plaats</b>
+                <p>Arnhem</p>
+
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include INCLUDES . 'footer.inc.php'; ?>
+<?php include INCLUDES . 'foot.inc.php'; ?>
