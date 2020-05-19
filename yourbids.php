7028f7e600eb6e5be0d0d905b3d06edfcd7a816c
BurnedSausage <kseverijns@outlook.com>
2020-05-19 14:37:05 +0200

    UPDATE yourBids.php

--- yourbids.php
+++ yourbids.php
@@ -4,33 +4,35 @@ include INCLUDES . 'head.inc.php';
 ?>
 
 <main>
-    <div class="ui stackable five column grid">
-        <?php
+    <div class="ui container">
+        <div class="ui stackable five column grid">
+            <?php
 
-        //getting all bids
-        $yourBids = Database::getInstance()->query("SELECT * FROM Bids WHERE username = '".Session::get('username')."' ORDER BY date");
+            //getting all bids
+            $yourBids = Database::getInstance()->query("SELECT * FROM Bids WHERE username = '".Session::get('username')."' ORDER BY date");
 
-        if ($yourBids->count() <= 0) {
-            // no data passed by get
-            echo "<p>U heeft geen lopende boden gedaan</p>";
-        }
+            if ($yourBids->count() <= 0) {
+                // no data passed by get
+                echo "<p>U heeft geen lopende boden gedaan</p>";
+            }
 
-        foreach($yourBids->results() as $yourBid) {
-            $result = Database::getInstance()->query("SELECT * FROM Items WHERE id = $yourBid");
-            ?>
-            <div class="column">
-                <div class="ui fluid card productcards">
-                    <a class="image" href="product.php?p=<?= $result->first()->id; ?>">
-                        <img src="<?= ROOT . $result->first()->thumbnail; ?>" alt="Foto van <?= $result->first()->title; ?>">
-                    </a>
-                    <div class="content">
-                        <a class="header" href="product.php?p=<?= $result->first()->id; ?>"><?= $result->first()->title; ?></a>
-                        <div class="description"><?= $result->first()->description; ?></div>
-                        <div class="description bold">€<?= $yourBid->amount; ?></div>
+            foreach($yourBids->results() as $yourBid) {
+                $result = Database::getInstance()->query("SELECT * FROM Items WHERE id = $yourBid->item");
+                ?>
+                <div class="column">
+                    <div class="ui fluid card productcards">
+                        <a class="image" href="product.php?p=<?= $result->first()->id; ?>">
+                            <img src="<?= ROOT . $result->first()->thumbnail; ?>" alt="Foto van <?= $result->first()->title; ?>">
+                        </a>
+                        <div class="content">
+                            <a class="header" href="product.php?p=<?= $result->first()->id; ?>"><?= $result->first()->title; ?></a>
+                            <div class="description"><?= $result->first()->description; ?></div>
+                            <div class="description bold">€<?= $yourBid->amount; ?></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
+        </div>
     </div>
 </main>
 
