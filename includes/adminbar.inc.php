4349c9cdf8a38f847da538190ea50ef66bde0e7e
Patrick Roelofs <contact@patrickroelofs.com>
2020-05-20 16:48:14 +0200

    ADD Adminbar styling and link to adminpanel

--- includes/adminbar.inc.php
+++ includes/adminbar.inc.php
@@ -1,3 +1,24 @@
-<?php
-    echo 'Admin logged in';
-?>
+<div class="adminbar">
+    <div class="ui container">
+        <div class="ui two column grid">
+            <div class="column left aligned">
+                <div class="admintitle">
+                    <em>Administrator ingelogd:</em>
+                    <strong>
+                        <?php
+                        if(empty($user->first()->firstname) || empty($user->first()->lastname)) {
+                            echo escape($user->first()->username);
+                        } else {
+                            echo escape($user->first()->firstname) . ' ' . escape($user->first()->lastname);
+                        }
+                        ?>
+                    </strong>
+                </div>
+            </div>
+
+            <div class="column right aligned">
+                <a target="_blank" href="admin/index.php" class="ui inverted button">Admin Paneel</a>
+            </div>
+        </div>
+    </div>
+</div>
