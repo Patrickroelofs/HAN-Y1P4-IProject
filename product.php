124f91067cf8ee165147b48265fd5182408ad170
Rowan-Paul <rpflynn80@gmail.com>
2020-05-26 11:48:22 +0200

    FIX 'bid' button

--- product.php
+++ product.php
@@ -159,7 +159,14 @@ include FUNCTIONS . 'admin.func.php';
                             </p>
                             <!-- Bidding -->
                             <?php // check if the user isn't the same as the trader
-                            if (strtolower(Session::exists('username')) != strtolower($thisItem->first()->trader)) { ?>
+                            if (Session::exists('username') && strtolower(Session::get('username')) != strtolower($thisItem->first()->trader)) { ?>
+                                <div class="ui input labeled input">
+                                    <button type="submit" id="makeOffer" class="ui primary labeled icon button">
+                                        <i class="gavel icon"></i>
+                                        Bieden
+                                    </button>
+                                </div>
+                            <?php } elseif (!Session::exists('username')) { ?>
                                 <div class="ui input labeled input">
                                     <button type="submit" id="makeOffer" class="ui primary labeled icon button">
                                         <i class="gavel icon"></i>
