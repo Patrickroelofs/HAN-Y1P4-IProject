2d71567e0cd5e198423a4c669db43812cb504584
Patrick Roelofs <contact@patrickroelofs.com>
2020-05-29 13:03:24 +0200

    ADD won products to foryou page

--- foryou.php
+++ foryou.php
@@ -6,16 +6,17 @@
         Redirect::to('index.php');
     }
 
-    // Get user products from database
-    $userProducts = Database::getInstance()->query("SELECT TOP 5 * FROM Items WHERE trader = '". Session::get('username') . "'", array());
-?>
+    ?>
 
 <main>
     <div class="ui container">
         <?php if($user->first()->trader) { ?>
-        <h2 id="yourSoldAuctions">Gesloten advertenties</h2>
+        <h2 id="yourSoldAuctions">Gesloten advertenties</h2><br>
         <div class="ui stackable five column grid">
-            <?php foreach($userProducts->results() as $result) {
+            <?php
+            $usertraderProducts = Database::getInstance()->query("SELECT * FROM Items WHERE trader = '". Session::get('username') . "' ORDER BY durationbegindate, durationbegintime DESC", array());
+
+            foreach($usertraderProducts->results() as $result) {
                 if($result->closed) {
                     ?>
 
@@ -32,17 +33,57 @@
                     </div>
 
                     <?php
-                } else { echo '<p>Geen gesloten advertenties...</p><br><br><br>'; }
+                }
+            }
+
+            if ($usertraderProducts->count() <= 0) {
+                echo '<p>Geen gesloten advertenties gevonden...</p>';
             }
             ?>
         </div>
         <?php } ?>
 
-        <?php if($user->first()->trader && !$userProducts->count() <= 0) { ?>
+        <?php if (!$user->first()->trader) {
+            $userbuyerProducts = Database::getInstance()->query("SELECT * FROM Items WHERE buyer = '". Session::get('username') . "' ORDER BY durationenddate, durationendtime DESC", array());
+            ?>
+
+            <h2 id="boughtAuctions">Gekochte advertenties</h2><br>
+            <div class="ui stackable five column grid">
+                <?php foreach($userbuyerProducts->results() as $result) {
+                    if($result->closed) {
+                        ?>
+
+                        <div class="column">
+                            <div class="ui fluid card">
+                                <a class="image" href="product.php?p=<?= $result->id; ?>">
+                                    <img src="<?= ROOT . $result->thumbnail; ?>" alt="Foto van <?= $result->title; ?>">
+                                </a>
+                                <div class="content">
+                                    <a class="header" href="product.php?p=<?= $result->id; ?>"><?= $result->title; ?></a>
+                                    <div class="description">Verkocht voor <span class="bold">€ <?= $result->saleprice ?></span></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php
+                    }
+                }
+
+                if ($userbuyerProducts->count() <= 0) {
+                    echo '<p>Geen gekochte producten gevonden...</p>';
+                }
+                ?>
+            </div>
+
+        <?php
+        }
+        ?>
+
+        <?php if($user->first()->trader && !$usertraderProducts->count() <= 0) { ?>
         <h2 id="yourAuctions">Lopende advertenties</h2>
         <div class="ui stackable five column grid">
             <div class="five column row">
-                <?php foreach ($userProducts->results() as $result) {
+                <?php foreach ($usertraderProducts->results() as $result) {
                     // Calculate time left in offer
                     $currentDate = new DateTime(date("Y-m-d"));
                     $endDate = new DateTime($result->durationenddate);
@@ -71,113 +112,6 @@
             </div>
         </div>
     <?php } ?>
-
-        <!--Aanbevolen rubrieken voor jou-->
-        <h2>Aanbevolen rubrieken voor jou</h2>
-        <div class="ui stackable four column grid">
-            <div class="column">
-                <div class="ui fluid card">
-                    <a class="image" href="#">
-                        <img src="https://place-hold.it/300x150" alt="rubriek-image">
-                    </a>
-                    <div class="content">
-                        <a class="header" href="#">iPhones</a>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui fluid card">
-                    <a class="image" href="#">
-                        <img src="https://place-hold.it/300x150" alt="rubriek-image">
-                    </a>
-                    <div class="content">
-                        <a class="header" href="#">Auto's</a>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui fluid card">
-                    <a class="image" href="#">
-                        <img src="https://place-hold.it/300x150" alt="rubriek-image">
-                    </a>
-                    <div class="content">
-                        <a class="header" href="#">Laptops</a>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui fluid card">
-                    <a class="image" href="#">
-                        <img src="https://place-hold.it/300x150" alt="rubriek-image">
-                    </a>
-                    <div class="content">
-                        <a class="header" href="#">Mondmaskers</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!--Aanbevolen producten-->
-        <h2>Aanbevolen producten</h2>
-        <div class="ui stackable five column grid">
-            <div class="five column row">
-                <div class="column">
-                    <div class="ui fluid card">
-                        <a class="image" href="#">
-                            <img src="https://place-hold.it/150x150" alt="product-image">
-                        </a>
-                        <div class="content">
-                            <a class="header" href="#">Product</a>
-                            <div class="description">€52,00</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="ui fluid card">
-                        <a class="image" href="#">
-                            <img src="https://place-hold.it/150x150" alt="product-image">
-                        </a>
-                        <div class="content">
-                            <a class="header" href="#">Product</a>
-                            <div class="description">€52,00</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="ui fluid card">
-                        <a class="image" href="#">
-                            <img src="https://place-hold.it/150x150" alt="product-image">
-                        </a>
-                        <div class="content">
-                            <a class="header" href="#">Product</a>
-                            <div class="description">€52,00</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="ui fluid card">
-                        <a class="image" href="#">
-                            <img src="https://place-hold.it/150x150" alt="product-image">
-                        </a>
-                        <div class="content">
-                            <a class="header" href="#">Product</a>
-                            <div class="description">€52,00</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="ui fluid card">
-                        <a class="image" href="#">
-                            <img src="https://place-hold.it/150x150" alt="product-image">
-                        </a>
-                        <div class="content">
-                            <a class="header" href="#">Product</a>
-                            <div class="description">€52,00</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </main>
 
