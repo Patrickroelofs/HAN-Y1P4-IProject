b5c48372b9f78f61c3782fe197bf34db70a1e313
Patrick Roelofs <contact@patrickroelofs.com>
2020-05-29 17:21:46 +0200

    UPDATE Profilepage add pagination

--- profile.php
+++ profile.php
@@ -5,14 +5,15 @@ include FUNCTIONS . 'profile.func.php';
 include INCLUDES . 'head.inc.php';
 
 // These statements have been seperated because they error on production server
-// If $_GET user is not a numeric character
-if(!is_numeric($_GET['user'])) {
+$userID = escape($_GET['user']);
+
+if(!is_numeric($userID)) {
     Message::error('index.php', array(
         'm' => 'Gebruiker bestaat niet'
     ));
 
 // If $_GET user is empty
-} else if(empty($_GET['user'])) {
+} else if(empty($userID)) {
     Message::error('index.php', array(
         'm' => 'Gebruiker bestaat niet'
     ));
@@ -21,7 +22,7 @@ if(!is_numeric($_GET['user'])) {
 
 
 //Get data from this user
-$thisUser = Database::getInstance()->query("SELECT * FROM Users WHERE id = '". escape($_GET['user']) ."'");
+$thisUser = Database::getInstance()->query("SELECT * FROM Users WHERE id = '". $userID ."'");
 
 // Check if user is banned
 if ($thisUser->first()->banned == true && Admin::isLoggedIn() == false) {
@@ -34,6 +35,103 @@ if ($thisUser->count() < 1) {
     Message::error('index.php', array(
         'm' => 'Gebruiker bestaat niet'
     ));
+}
+
+$data = '';
+$data .= '?user='.$userID;
+
+if(isset($_GET['itemOffset'])) {
+    if(!empty($_GET['itemOffset'])) {
+        $itemOffset = $_GET['itemOffset'];
+    } else {
+        $itemOffset = 0;
+    }
+} else {
+    $itemOffset = 0;
+}
+
+if(isset($_GET['bidsOffset'])) {
+    if(!empty($_GET['bidsOffset'])) {
+        $bidsOffset = $_GET['bidsOffset'];
+    } else {
+        $bidsOffset = 0;
+    }
+} else {
+    $bidsOffset = 0;
+}
+
+if(isset($_GET['reviewsOffset'])) {
+    if(!empty($_GET['reviewsOffset'])) {
+        $reviewsOffset = $_GET['reviewsOffset'];
+    } else {
+        $reviewsOffset = 0;
+    }
+} else {
+    $reviewsOffset = 0;
+}
+
+
+if(isset($_POST['submit-offset-items-up'])) {
+    $data .= '&itemOffset=' . $itemOffset += 10;
+    $data .= '&bidsOffset=' . $bidsOffset;
+    $data .= '&reviewsOffset=' . $reviewsOffset;
+
+    Redirect::to('profile.php' . $data);
+
+} else if (isset($_POST['submit-offset-items-down'])) {
+    $data .= '&itemOffset=' . $itemOffset -= 10;
+    $data .= '&bidsOffset=' . $bidsOffset;
+    $data .= '&reviewsOffset=' . $reviewsOffset;
+
+    Redirect::to('profile.php' . $data);
+
+} else if (isset($_POST['submit-offset-bids-up'])) {
+    $data .= '&itemOffset=' . $itemOffset;
+    $data .= '&bidsOffset=' . $bidsOffset += 5;
+    $data .= '&reviewsOffset=' . $reviewsOffset;
+
+    Redirect::to('profile.php' . $data);
+
+} else if (isset($_POST['submit-offset-bids-down'])) {
+    $data .= '&itemOffset=' . $itemOffset;
+    $data .= '&bidsOffset=' . $bidsOffset -= 5;
+    $data .= '&reviewsOffset=' . $reviewsOffset;
+
+    Redirect::to('profile.php' . $data);
+
+} else if (isset($_POST['submit-offset-review-up'])) {
+    $data .= '&itemOffset=' . $itemOffset;
+    $data .= '&bidsOffset=' . $bidsOffset;
+    $data .= '&reviewsOffset=' . $reviewsOffset += 5;
+
+    Redirect::to('profile.php' . $data);
+
+} else if (isset($_POST['submit-offset-review-down'])) {
+    $data .= '&itemOffset=' . $itemOffset;
+    $data .= '&bidsOffset=' . $bidsOffset;
+    $data .= '&reviewsOffset=' . $reviewsOffset -= 5;
+
+    Redirect::to('profile.php' . $data);
+}
+
+if(isset($_GET['itemOffset']) && isset($_GET['bidsOffset']) && isset($_GET['reviewsOffset'])) {
+    $getAllItems    = Database::getInstance()->query("SELECT hidden, id, title, description, price, thumbnail FROM Items WHERE trader = '". $thisUser->first()->username ."' ORDER BY title OFFSET $itemOffset ROWS FETCH NEXT 10 ROWS ONLY", array());
+    $getAllBids     = Database::getInstance()->query("SELECT item, amount FROM Bids WHERE username = '". $thisUser->first()->username ."' ORDER BY date, time DESC OFFSET $bidsOffset ROWS FETCH NEXT 5 ROWS ONLY", array());
+    $getAllReviews  = Database::getInstance()->query("SELECT review, comment, date, time FROM Feedback WHERE username = '". $thisUser->first()->username ."' ORDER BY date,time DESC OFFSET $reviewsOffset ROWS FETCH NEXT 5 ROWS ONLY", array());
+
+    $countItems     = Database::getInstance()->query("SELECT id FROM Items WHERE trader = '". $thisUser->first()->username ."'");
+    $countBids      = Database::getInstance()->query("SELECT item FROM Bids WHERE username = '". $thisUser->first()->username ."'");
+    $countFeedback  = Database::getInstance()->query("SELECT item FROM Feedback WHERE username = '" . $thisUser->first()->username . "'");
+
+} else {
+    $getAllItems    = Database::getInstance()->query("SELECT hidden, id, title, description, price, thumbnail FROM Items WHERE trader = '". $thisUser->first()->username ."' ORDER BY title OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY", array());
+    $getAllBids     = Database::getInstance()->query("SELECT item, amount FROM Bids WHERE username = '". $thisUser->first()->username ."' ORDER BY date, time DESC OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY", array());
+    $getAllReviews  = Database::getInstance()->query("SELECT review, comment, date, time FROM Feedback WHERE username = '". $thisUser->first()->username ."' ORDER BY date,time DESC OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY", array());
+
+    $countItems     = Database::getInstance()->query("SELECT id FROM Items WHERE trader = '". $thisUser->first()->username ."'");
+    $countBids      = Database::getInstance()->query("SELECT item FROM Bids WHERE username = '". $thisUser->first()->username ."'");
+    $countFeedback  = Database::getInstance()->query("SELECT item FROM Feedback WHERE username = '" . $thisUser->first()->username . "'");
+
 }
 
 include FUNCTIONS . 'admin.func.php';
@@ -94,95 +192,160 @@ include FUNCTIONS . 'admin.func.php';
             <br>
             <div class="ui stackable five column grid">
                 <?php
-                $products = Database::getInstance()->query("SELECT * FROM Items WHERE trader = '". $thisUser->first()->username ."'");
 
-                if ($products->count() <= 0) {
+                if ($getAllItems->count() <= 0) {
                     // no data passed by get
                     echo "<p>Geen producten gevonden...</p>";
                 }
 
-                foreach($products->results() as $result) {
+                foreach($getAllItems->results() as $result) {
                     if(!$result->hidden || Admin::isLoggedIn()) {
-                    ?>
-                    <div class="column">
-                        <div class="ui fluid card productcards <?php if($result->hidden) { echo 'itemhidden'; } ?>">
-                            <a class="image" href="product.php?p=<?= $result->id; ?>">
-                                <img src="<?= ROOT . $result->thumbnail; ?>" alt="Foto van <?= escape($result->title); ?>">
-                            </a>
-                            <div class="content">
-                                <a class="header" href="product.php?p=<?= $result->id; ?>"><?= escape($result->title); ?></a>
-                                <div class="description"><?= escape(Modifiers::textlength($result->description, 100)); ?>...</div>
-                                <div class="description bold">€<?= escape($result->price); ?></div>
+                        ?>
+                        <div class="column">
+                            <div class="ui fluid card productcards <?php if($result->hidden) { echo 'itemhidden'; } ?>">
+                                <a class="image" href="product.php?p=<?= $result->id; ?>">
+                                    <img src="<?= ROOT . $result->thumbnail; ?>" alt="Foto van <?= escape($result->title); ?>">
+                                </a>
+                                <div class="content">
+                                    <a class="header" href="product.php?p=<?= $result->id; ?>"><?= escape($result->title); ?></a>
+                                    <div class="description"><?= escape(Modifiers::textlength($result->description, 100)); ?>...</div>
+                                    <div class="description bold">€<?= escape($result->price); ?></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php }
+                    <?php }
                 } ?>
             </div>
+            <br>
+            <form class="ui large form" action="" method="post">
+                <?php if(isset($_GET['itemOffset'])) {
+                    if($_GET['itemOffset'] > 0) {
+                        ?>
+                        <button type="submit" class="ui left labeled icon button primary" name="submit-offset-items-down">
+                            <i class="left arrow icon"></i>
+                            Vorige pagina
+                        </button>
+                        <?php
+                    }
+                }
+
+                if($countItems->count() >= $itemOffset && $countItems->count() - $itemOffset >= 5) {
+                    ?>
+                    <button type="submit" class="ui right labeled icon button primary" name="submit-offset-items-up">
+                        <i class="right arrow icon"></i>
+                        Volgende pagina
+                    </button>
+                    <?php
+                }
+                ?>
+            </form>
         </div>
 
+
+        <br><br>
         <div class="profileDivider">
             <h2>Recente biedingen van deze gebruiker</h2>
             <div>
-                <?php
-                    $bids = Database::getInstance()->query("SELECT TOP 10 * FROM Bids WHERE username = '". $thisUser->first()->username ."' ORDER BY date DESC");
-
-                    if($bids->count() <= 0) {
-                        echo '<p>Geen biedingen gevonden...</p>';
-                    }
-                ?>
                 <ul class="bidlist">
-                <?php
-                    foreach($bids->results() as $bid) {
-                        $bidBlocked = Database::getInstance()->query("SELECT * FROM Items WHERE id = '$bid->item'",array());
-                        if($bidBlocked->first()->hidden == true) {
-                            echo "Geen biedingen gevonden";
-                        } else {
-                        ?>
-                        <li>
+                    <?php
+                    foreach($getAllBids->results() as $bid) {
+                            ?>
+                            <li>
                             <span>
                                 <a href="product.php?p=<?= $bid->item; ?>">
-                                    <?php $item = Database::getInstance()->query("SELECT * FROM Items where id = '". $bid->item ."'");
-                                        echo substr($item->first()->title, 0, 25);
+                                    <?php $item = Database::getInstance()->query("SELECT title FROM Items where id = '". $bid->item ."'");
+                                    echo substr($item->first()->title, 0, 25);
                                     ?>
                                 </a>
                             </span>
 
-                            <span>€ <?= $bid->amount; ?></span></li>
-                        <?php
-                    } }
-                ?>
+                                <span>€ <?= $bid->amount; ?></span></li>
+                            <?php
+                        }
+
+                    if($getAllBids->count() <= 0) {
+                        echo 'Geen biedingen gevonden...';
+                    }
+                    ?>
                 </ul>
+                <form class="ui large form" action="" method="post">
+                    <?php if(isset($_GET['bidsOffset'])) {
+                        if($_GET['bidsOffset'] > 0) {
+                            ?>
+                            <button type="submit" class="ui left labeled icon button primary" name="submit-offset-bids-down">
+                                <i class="left arrow icon"></i>
+                                Vorige pagina
+                            </button>
+                        <?php
+                        }
+                    }
+
+                    if($countBids->count() >= $bidsOffset && $countBids->count() - $bidsOffset >= 5) {
+                        ?>
+                        <button type="submit" class="ui right labeled icon button primary" name="submit-offset-bids-up">
+                            <i class="right arrow icon"></i>
+                            Volgende pagina
+                        </button>
+                        <?php
+                    }
+                    ?>
+                </form>
             </div>
         </div>
+        <br><br>
         <div class="profileDivider">
-            <h2>Recente reviews van deze gebruiker</h2>
-            <?php
-            $step = 5;
-            if(isset($_GET['amount'])) {
-                $reviewAmount = $_GET['amount'];
-            } else {
-                $reviewAmount = 5;
+            <h2>Recente feedback op deze gebruiker</h2>
+            <?php foreach ($getAllReviews->results() as $result) {
+
+                ?>
+
+                <div class="ui segment">
+                    <h3><?= $result->review ?></h3>
+                    <p>
+                        <?php if (empty($result->comment)) {
+                            echo '<em>Geen bericht achtergelaten.</em>';
+                        } else {
+                            echo $result->comment;
+                        }
+                        ?>
+                    </p>
+                    <p>
+                        <?= $result->date; ?>
+                        <?= substr($result->time, 0, 5); ?>
+                    </p>
+                </div>
+
+                <?php
             }
-            $allReviews = Database::getInstance()->query("SELECT * FROM Feedback WHERE username= '". $thisUser->first()->username ."' ORDER BY date DESC");
-            $reviews = Database::getInstance()->query("SELECT TOP $reviewAmount * FROM Feedback where username = '". $thisUser->first()->username ."' ORDER BY date DESC");
-            if($reviews->count() <= 0) {
-                echo '<p>Geen reviews gevonden...</p>';
+
+            if($getAllReviews->count() <= 0) {
+                echo 'Geen feedback gevonden...';
             }
             ?>
-            <?php
-            // Display the top 5 reviews from the user
-            foreach ($reviews->results() as $review) {?>
-            <div class="ui segment">
-                <h3><?= ucfirst($review->review) ?></h3>
-                <p><?= ucfirst($review->comment) ?></p>
-                <p><?= $review->date ?></p>
-            </div>
-                <?php if($allReviews->count() > $reviewAmount) {?>
-            <div>
-                <a href="profile.php?user= <?= $_GET['user'] ?>&amount= <?= $reviewAmount + $step ?> "></a>
-            </div>
-            <?php }}?>
+
+
+            <form class="ui large form" action="" method="post">
+                <?php if(isset($_GET['reviewsOffset'])) {
+                    if($_GET['reviewsOffset'] > 0) {
+                        ?>
+                        <button type="submit" class="ui left labeled icon button primary" name="submit-offset-review-down">
+                            <i class="left arrow icon"></i>
+                            Vorige pagina
+                        </button>
+                        <?php
+                    }
+                }
+
+                if($countFeedback->count() >= $reviewsOffset && $countFeedback->count() - $reviewsOffset >= 5) {
+                    ?>
+                    <button type="submit" class="ui right labeled icon button primary" name="submit-offset-review-up">
+                        <i class="right arrow icon"></i>
+                        Volgende pagina
+                    </button>
+                    <?php
+                }
+                ?>
+            </form>
         </div>
     </div>
 </main>
