ec760dac61c1b623bbfb4636a39fbccd5c044e24
Patrick Roelofs <contact@patrickroelofs.com>
2020-06-02 11:53:24 +0200

    Revert "ADD trader can not bid on any item"
    This reverts commit 4756df94f2dc59f40381bc3b496ce994e778b8ac.

--- product.php
+++ product.php
@@ -174,7 +174,6 @@ include FUNCTIONS . 'admin.func.php';
                             </p>
                             <!-- Bidding -->
                             <?php // check if the user isn't the same as the trader
-                            if(!$user->first()->trader) {
                             if (Session::exists('username') && strtolower(Session::get('username')) != strtolower($thisItem->first()->trader)) { ?>
                                 <div class="ui input labeled input">
                                     <button type="submit" id="makeOffer" class="ui primary labeled icon button">
@@ -189,9 +188,7 @@ include FUNCTIONS . 'admin.func.php';
                                         Bieden
                                     </button>
                                 </div>
-                            <?php }
-                            } else {
-                                echo 'Een verkoper kan geen biedingen uitbrengen, gebruik een kopers-account <br><br>'; } ?>
+                            <?php } ?>
                             <!-- Contact -->
                             <?php
                             if(Session::exists('username')) {
