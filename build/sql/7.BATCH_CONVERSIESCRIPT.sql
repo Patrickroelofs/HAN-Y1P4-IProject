33c9efe2b751fbbfbc57f1b3b9a5eea098792d36
Patrick Roelofs <contact@patrickroelofs.com>
2020-06-02 16:06:27 +0200

    ADD description conversion | RUNNING .bat WILL TAKE 20 MINUTES ON LOCAL

--- build/sql/7.BATCH_CONVERSIESCRIPT.sql
+++ build/sql/7.BATCH_CONVERSIESCRIPT.sql
@@ -67,7 +67,7 @@ SELECT
     ID,
     Verkoper,
     LEFT(Titel, 255),
-    '',
+    Beschrijving,
     concat('https://iproject19.icasites.nl/thumbnails/',Thumbnail),
     Categorie,
     Prijs,
@@ -88,6 +88,86 @@ FROM batchItems
 SET IDENTITY_INSERT Items OFF
 GO
 
+DROP FUNCTION IF EXISTS dbo.stripHTML
+GO
+
+CREATE FUNCTION dbo.stripHTML (@HTMLText varchar(MAX))
+    RETURNS varchar(MAX)
+AS
+BEGIN
+    DECLARE @Start  int
+    DECLARE @End    int
+    DECLARE @Length int
+
+    -- **DELETE** everything between <STYLE AND </STYLE>
+    SET @Start = CHARINDEX('<STYLE', @HTMLText)
+    SET @End = CHARINDEX('</STYLE>', @HTMLText, CHARINDEX('<', @HTMLText)) + 7
+    SET @Length = (@End - @Start) + 1
+
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '')
+        SET @Start = CHARINDEX('<STYLE', @HTMLText)
+        SET @End = CHARINDEX('</STYLE>', @HTMLText, CHARINDEX('</STYLE>', @HTMLText)) + 7
+        SET @Length = (@End - @Start) + 1
+    END
+
+    -- **DELETE** everything between <SCRIPT AND </SCRIPT>
+    SET @Start = CHARINDEX('<SCRIPT', @HTMLText)
+    SET @End = CHARINDEX('</SCRIPT>', @HTMLText, CHARINDEX('<', @HTMLText)) + 7
+    SET @Length = (@End - @Start) + 1
+
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '')
+        SET @Start = CHARINDEX('<SCRIPT', @HTMLText)
+        SET @End = CHARINDEX('</SCRIPT>', @HTMLText, CHARINDEX('</SCRIPT>', @HTMLText)) + 7
+        SET @Length = (@End - @Start) + 1
+    END
+
+    -- **REPLACE** &bnsp; with a SPACE
+    SET @Start = CHARINDEX('&nbsp;', @HTMLText)
+    SET @End = @Start + 5
+    SET @Length = (@End - @Start) + 1
+
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('&nbsp;', @HTMLText)
+        SET @End = @Start + 5
+        SET @Length = (@End - @Start) + 1
+    END
+
+    -- **REPLACE** &amp; with a &
+    SET @Start = CHARINDEX('&amp;', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '&')
+        SET @Start = CHARINDEX('&amp;', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+
+
+    -- **DELETE** everything between < and >
+    SET @Start = CHARINDEX('<', @HTMLText)
+    SET @End = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+    SET @Length = (@End - @Start) + 1
+
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '')
+        SET @Start = CHARINDEX('<', @HTMLText)
+        SET @End = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+        SET @Length = (@End - @Start) + 1
+    END
+
+    RETURN LTRIM(RTRIM(@HTMLText))
+
+END
+GO
+
+update Items
+set description = dbo.stripHTML (description)
+GO
 
 INSERT INTO Files (ITEM, FILENAME)
 SELECT ItemID,
